646bf9fe3cbb0819086c9a05660bc6f980de9ba2
P110136416 <p110136416@student.peterborough.ac.uk>
2021-11-10 15:55:38 +0000

    Installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import AppHeader from './AppHeader'
 import AppPageTitle from './AppPageTitle'
 import AppHomepage from './AppHomepage'
