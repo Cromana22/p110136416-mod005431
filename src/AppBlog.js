263592029f3fe499f6ad30f120ed5bc9030f2fe6
P110136416 <p110136416@student.peterborough.ac.uk>
2021-11-11 09:54:41 +0000

    Added blog frame and additional routing

--- src/AppBlog.js
+++ src/AppBlog.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import AppPageTitle from './AppPageTitle'
+import BlogList from './BlogList';
+import { Outlet } from "react-router-dom";
 
 function AppBlog() {
     return (
       <div className="AppBlog">
         <AppPageTitle title="Get Baking! Blog" />
-        <p>This is the blog</p>
+        <BlogList />
+        <Outlet />
       </div>
     )
   }
---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function BlogList() {
+    return (
+      <div className="BlogList">
+        <ul>
+            <li><Link to="blog1">Blog list 1</Link></li>
+            <li><Link to="blog2">Blog list 2</Link></li>
+            <li><Link to="blog3">Blog list 3</Link></li>
+            <li><Link to="blog4">Blog list 4</Link></li>
+        </ul>
+      </div>
+    )
+  }
+  
+  export default BlogList
