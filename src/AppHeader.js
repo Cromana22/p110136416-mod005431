11fcc16506cabf5856f631a119df37467df4c58a
P110136416 <p110136416@student.peterborough.ac.uk>
2021-11-26 11:43:47 +0000

    made main title clickable home link

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -6,10 +6,10 @@ function AppHeader() {
     return (
       <div className="AppHeader">
         <section className="valign-wrapper row">
-          <div className="container col s4 left-align">
-            <a className="left" href="/home"><img src={logo} alt="The Get Baking Logo" height="200"/></a>
-          </div>
-          <div className="container col s4"><h1 className="center-align yellow-text text-accent-1">Get Baking!</h1></div>
+          <div className="container col s4 left-align"><img src={logo} alt="The Get Baking Logo" height="200"/></div>
+          <a className="container col s4" href="/home">
+            <div><h1 className="center-align yellow-text text-accent-1">Get Baking!</h1></div>
+          </a>
           <div className="container col s4 right-align"><img src={logo} alt="The Get Baking Logo" height="200"/></div>
         </section>
       </div>
