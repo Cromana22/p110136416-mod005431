0de05fe189e2f4eec2bd5967895d13d4c9666aad
P110136416 <p110136416@student.peterborough.ac.uk>
2021-12-01 13:31:27 +0000

    blog styling adjustment

--- src/AppBlog.js
+++ src/AppBlog.js
@@ -3,6 +3,7 @@ import AppPageTitle from './AppPageTitle'
 import useFetch from './scripts/useFetch.js';
 import BlogLinks from './BlogLinks';
 import { Outlet } from 'react-router';
+import './AppBlog.css';
 
 function AppBlog() {
   const { response, loading, error } = useFetch("https://p110136416-6.free.beeceptor.com/blogs");
@@ -12,14 +13,13 @@ function AppBlog() {
       <AppPageTitle title="Get Baking Blog" />
       <div className="pink lighten-4 deep-purple-text row">
 
-        <div className="col s12 m3">
+        <div className="col s12 m3 right-border pink lighten-5">
           <ul>
             { loading && <p>{loading}</p> } {/* if loading, output loading */}
             { error && <p>{error}</p> } {/* if error, output error */}
             { response && <BlogLinks blogs={response} /> } {/* if loaded, put data into Bloglist as a prop */}
           </ul>
         </div>
-
         <div className="col s12 m9">
           <Outlet />
         </div>
--- src/AppRecipes.js
+++ src/AppRecipes.js
@@ -2,16 +2,24 @@ import React from 'react';
 import AppPageTitle from './AppPageTitle';
 import { Link } from "react-router-dom";
 import RecipeGrid from './RecipeGrid';
+import useFetch from './scripts/useFetch.js';
 
 function AppRecipes() {
+  const { response, loading, error } = useFetch("https://p110136416-6.free.beeceptor.com/recipes");
+
     return (
       <div className="AppRecipeList">
         <AppPageTitle title="Find Great Recipes" />
         <div className="pink lighten-4 deep-purple-text">
-          <div className="container"> 
+          <div>
             <p>Search fields</p>
+          </div>
+
+          <div className="container"> 
             <Link to="addRecipe">Contribute a recipe</Link>
-            <RecipeGrid cards={12} />
+            { loading && <p>{loading}</p> } {/* if loading, output loading */}
+            { error && <p>{error}</p> } {/* if error, output error */}
+            { response && <RecipeGrid cards={response} /> } {/* if loaded, put data into recipe grid as a prop */}
           </div>
         </div>
       </div>
